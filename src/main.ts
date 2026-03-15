9a86ba187abd6c99afe97f9c07f6dafcb64bfd2a
Walter Iamartino <walteriamartino@gmail.com>
2026-03-15 23:54:22 +0100

    port change for new front

--- src/main.ts
+++ src/main.ts
@@ -70,7 +70,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
     credentials: true,
   });
 
